5c6b10dbc4796b824d05d0343515f15a02ba165f
Nicholaus Rowe <nrowe@DumpsterFire.local>
2020-12-23 03:43:38 -0800

    Add regex for OTC titles to save as Windows filename

--- api/convertURL.js
+++ api/convertURL.js
@@ -4,15 +4,19 @@ const path = require('path');
 
 module.exports = async function convertURL(passedInURL) {
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(passedInURL, {
-      waitUntil: 'networkidle2',
-    });
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(passedInURL, {
+    waitUntil: 'networkidle2',
+  });
 
-  const fileName = await page._frameManager._mainFrame.evaluate(() => document.title);
+  let fileName = await page._frameManager._mainFrame.evaluate(() => document.title);
 
-  const pathToPDF = path.join(__dirname, `pdf/${fileName}.pdf`);
+  const chars = {'\|': '-', ',': ''};
+
+  fileName = fileName.replace(/[\|,]/g, key => chars[key]);
+
+  const pathToPDF = path.join(__dirname, `/pdf/${fileName}.pdf`);
 
   await page.pdf({
     path: pathToPDF,
