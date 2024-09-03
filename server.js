40db3e5e41d7f0f0a5d6844c4627e9c0384f9443
Nicholaus <ux@nicholausrowe.com>
2024-09-03 03:26:44 -0700

    Change Require to Import

--- server.js
+++ server.js
@@ -1,43 +1,36 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const convertURL = require('./api/convertURL.js');
-// const convertURL = require('./server/api/convertSimple.js');
-// const convertURL = require('./server/api/convertStreamSync.js');
-// var browserify = require('browserify');
-// var fs = require('browserify-fs');
-
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import convertURL from './api/convertURL.js';
 
 const app = express();
 
-const joinedPath = path.join(__dirname, 'public');
+const joinedPath = path.join(process.cwd(), 'public');
 const staticDir = express.static(joinedPath);
 
 app.use(staticDir);
 app.use(express.json());
 
 app.post('/', async (req, res, next) => {
-  var passedInURL = req.body.url;
+  const passedInURL = req.body.url;
   console.time(`convertDuration – ${passedInURL}`);
 
   try {
     const pathToPDF = await convertURL(passedInURL);
-    res.status(201)
-      .download(pathToPDF, pathToPDF, () => {
-        fs.unlink(pathToPDF, (err) => {
-          if (err) throw err;
-          console.log(`${pathToPDF} successfully deleted!`);
-          console.timeEnd(`convertDuration – ${passedInURL}`);
-
-        });
-      })
+    res.status(201).download(pathToPDF, pathToPDF, () => {
+      fs.unlink(pathToPDF, (err) => {
+        if (err) throw err;
+        console.log(`${pathToPDF} successfully deleted!`);
+        console.timeEnd(`convertDuration – ${passedInURL}`);
+      });
+    });
   } catch (err) {
     console.error('CATCH condition from app.post(): ', err);
     res.sendStatus(500);
   }
 });
 
-const PORT = process.env.PORT || 4000; // Use Heroku's environment variable PORT or fallback to 3000
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}!`);
