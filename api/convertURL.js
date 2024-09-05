5284ef841d964df9bf202ca5128af328fa0fcdbd
Nicholaus <ux@nicholausrowe.com>
2024-09-04 23:14:37 -0700

    Stuff

--- api/convertURL.js
+++ api/convertURL.js
@@ -43,6 +43,11 @@ async function convertURL(passedInURL) {
 
       //set User Agent
       await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+      //add User Agent Headers
+      await page.setExtraHTTPHeaders({
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br'
+      });
 
       await page.goto(passedInURL, {
         waitUntil: 'networkidle2',
