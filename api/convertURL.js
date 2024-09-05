4f3878b5ed57e6e3e01b38b40cabb4a6854d2519
Nicholaus <ux@nicholausrowe.com>
2024-09-04 23:09:51 -0700

    Stuff

--- api/convertURL.js
+++ api/convertURL.js
@@ -40,6 +40,10 @@ async function convertURL(passedInURL) {
       });
 
       const page = await browser.newPage();
+
+      //set User Agent
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
       await page.goto(passedInURL, {
         waitUntil: 'networkidle2',
         timeout: 120000,
